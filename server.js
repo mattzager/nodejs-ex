266578632393a925e828b915af3957a6e21cf81f
mattzager <demo@openshift.example.com>
2015-12-14 20:09:26 +0000

    update

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 app.get('/', function (req, res) {
 res.write('<html><body>');
-  res.write('<h1>Hello OpenShift v3!!!</h1>');
+  res.write('<h1>Hello OpenShift v3</h1>');
   res.write('<form action="/exit" method="get"><input type="submit" value="Healthcheck"></form>');
   res.write('<hr/>');
   var os = require('os');
